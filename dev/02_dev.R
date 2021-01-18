d94bc7b8671985beec17fb794475d334f9b57466
laurabiggins <laurabiggins10@gmail.com>
2021-01-18 12:33:03 +0000

    updated packages required

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -17,6 +17,7 @@
 ## Add one line by package you want to add as dependency
 usethis::use_package( "dplyr" )
 usethis::use_package( "DT" )
+usethis::use_package("assertthat")
 
 ## Add modules ----
 ## Create a module infrastructure in R/
