6f023c4a42cdf0d736349dbe288b80ca77a044d9
laurabiggins <laurabiggins10@gmail.com>
2021-03-25 09:30:04 +0000

    cleaned up violin plot code to remove processing from the module

--- R/mod_violinplot.R
+++ R/mod_violinplot.R
@@ -27,8 +27,8 @@ mod_violinplot_ui <- function(id, meta_sum){
           checkboxInput(ns("add_boxplot"), "add boxplot"),
           br(),
           downloadButton(ns("download_png"), "png"),
-          downloadButton(ns("download_pdf"), "pdf")#,
-         # actionButton(ns("browser"), "browser")
+          downloadButton(ns("download_pdf"), "pdf"),
+          actionButton(ns("browser"), "browser")
         ),
         mainPanel(
           width = 8,
@@ -57,22 +57,15 @@ mod_violinplot_ui <- function(id, meta_sum){
 #' scatterplot Server Function
 #'
 #' @noRd 
-mod_violinplot_server <- function(id, dataset, meta_sum, metadata, sample_name_col, prefix = "", session) {
+mod_violinplot_server <- function(id, long_data_tib, sample_name_col, prefix = "", session) {
   
   moduleServer(id, function(input, output, session) {
-    
-    tibble_dataset <- reactive(get_tibble_dataset(dataset, sample_name_col))
-    
-    selected_data <- reactive({
-      data_with_group_info(metadata, tibble_dataset(), input$select_condition, sample_name_col)
-    })
-    
+
     violin_obj <- reactive({
-      violinplot(selected_data(), input$select_condition, boxplot = input$add_boxplot)
+      violinplot(long_data_tib, input$select_condition, boxplot = input$add_boxplot)
     })
     
     output$plot <- renderPlot({
-      #violinplot(selected_data(), input$select_condition, boxplot = input$add_boxplot)
       violin_obj()
     }) %>% bindCache(input$select_condition, input$add_boxplot)
     
@@ -105,8 +98,6 @@ mod_violinplot_server <- function(id, dataset, meta_sum, metadata, sample_name_c
         )
       }
     )
-    
-    
     observeEvent(input$browser, browser())
   })
 }
@@ -114,8 +105,9 @@ mod_violinplot_server <- function(id, dataset, meta_sum, metadata, sample_name_c
 
 #' violinplot
 #'
-#' @param selected_data 
-#' @param condition 
+#' @param dataset entire dataset 
+#' @param condition selected condition to plot
+#' @param boxplot whether to add a boxplot on top of the violin plot (TRUE or FALSE)
 #'
 #' @return
 violinplot <- function(dataset, condition, boxplot = FALSE) {
@@ -129,44 +121,3 @@ violinplot <- function(dataset, condition, boxplot = FALSE) {
   }
   p
 }
-
-
-#' get_tibble_dataset
-#' 
-#' Convert the main matrix dataset into a tibble
-#'
-#' @param matrix_data main dataset
-#' @param sample_name_col column name that contains all the sample names (should
-#' be set in the initial golem options, then passed through as a function argument
-#' to the module)
-#'
-#' @return tibble
-get_tibble_dataset <- function(matrix_data, sample_name_col) {
-
-  tib_data <- tibble::as_tibble(matrix_data, rownames = "row_attribute")
-  tidyr::pivot_longer(tib_data, !row_attribute, names_to = sample_name_col)
-}
-
-#' select_by_group
-#' 
-#' This function adds the relevant metadata to the main dataset.
-#' 
-#' It removes NAs and coerces the condition to a factor. This is done locally here 
-#' as we need it for the violinplot but may not want to change the main metadata object.
-#'
-#' @param metadata the metadata tibble
-#' @param tibble_dataset the main dataset in tibble format
-#' @param condition the condition that we want to pull the groups from
-#' @param sample_name_col the name of the column that contains all the sample names
-#'
-#' @return 
-data_with_group_info <- function(metadata, tibble_dataset, condition, sample_name_col){
-  
-  metadata %>%
-    dplyr::select(c(condition, sample_name_col)) %>%
-    tidyr::drop_na(condition) %>%
-    dplyr::mutate_at(condition, factor) %>%
-    dplyr::inner_join(tibble_dataset) 
-} 
-# this could be simplified by just merging the tibbles and not bothering with the filtering,
-# but if the dataset is massive, it might be good to filter it.
