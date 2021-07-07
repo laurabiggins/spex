330af993b45da2a676dd8e75cfeda2061eaa14dc
laurabiggins <laurabiggins10@gmail.com>
2021-07-07 15:05:19 +0100

    Heatmap should now work

--- R/mod_heatmap.R
+++ R/mod_heatmap.R
@@ -1,5 +1,4 @@
-#!! This is not generic - it's been tweaked to look reasonable with the FemExpression 
-# data in the selected_data() reactive expression
+## If this doesn't show up straightaway in the app, try dev.off() in the console.
 
 #' histogram UI Function
 #'
@@ -10,12 +9,6 @@
 #' @noRd 
 #'
 #' @importFrom shiny NS tagList 
-# mod_histogram_ui <- function(id){
-#   ns <- NS(id)
-#   tagList(
-#  
-#   )
-# }
 mod_heatmap_ui <- function(id){
   
   ns <- NS(id)
@@ -26,8 +19,14 @@ mod_heatmap_ui <- function(id){
       fluidRow(
         column(2, downloadButton(ns("download_png"), "png")),
         column(2, downloadButton(ns("download_pdf"), "pdf")),
-        column(3, offset = 5,numericInput(ns("plot_height"), "plot height", 500))
-        #numericInput(ns("plot_width"), "plot width", 700),
+        column(3, 
+          selectInput(
+            inputId = ns("selected_set"),
+            label = "select set",
+            choices = ""
+          )
+        ),
+        column(3, offset = 2,numericInput(ns("plot_height"), "plot height", 500))
       ),
       shinycssloaders::withSpinner(
         plotOutput(ns("plot"), width = "100%", height = "500"), 
@@ -60,96 +59,96 @@ mod_heatmap_ui <- function(id){
 
 #'
 #' @noRd 
-mod_heatmap_server <- function(id, dataset, meta_sum, metadata, of_interest,
-                               sample_name_col, prefix = "", session) {
+mod_heatmap_server <- function(id, dataset, metadata, of_interest,
+                               sample_name_col, chosen_dataset, prefix = "", session) {
   moduleServer(
     id,
     function(input, output, session) {
       
-      # heatmap_options <- reactiveValues(
-      #   annot_col = tibble::column_to_rownames(metadata, "sample_name")#,
-      #   #annot_row = tibble::column_to_rownames(of_interest[[1]], "gene")
-      # )
-      # 
-      # # row_annotations <- reactive({
-      # #   tib <- dplyr::filter(
-      # #     of_interest[[1]], 
-      # #     gene %in% rownames(selected_data())
-      # #   )
-      # #   tibble::column_to_rownames(tib, "gene")
-      # # })
-      # 
-      # # this has to be in a reactive expression because the dataset 
-      # # passed to mod_heatmap_server is a reactive expression
-      # selected_data <- reactive({
-      #   
-      #   genes_of_interest <- dplyr::pull(of_interest[[1]], gene)
-      #   filtered_meta <- dplyr::filter(metadata, class %in% c("AA", "DA", "OEA"))
-      #   selected_samples <- dplyr::pull(filtered_meta, sample_name_col)
-      #   # we're working with a matrix so can't do dplyr
-      #   matrix_columns <- colnames(dataset()) %in% selected_samples
-      #   dataset()[rownames(dataset()) %in% genes_of_interest, matrix_columns]
-      # })
-      # 
-      # heatmap_obj <- reactive({
-      #   pheatmap::pheatmap(
-      #     selected_data(),
-      #     scale = "row",
-      #     annotation_col = heatmap_options$annot_col,
-      #     #annotation_row = row_annotations(),
-      #     silent = TRUE
-      #   )
-      # })
-      # 
-      # 
-      output$plot <- renderPlot({
-        plot(1:10)
+      ## update select input ----
+      observeEvent(of_interest(), {
+        opts <- c("all", names(of_interest()))
+        updateSelectInput(
+          inputId = "selected_set", 
+          choices = opts,
+          selected = dplyr::last(opts)
+        )
       })
-      # 
-      # output$plot <- renderPlot({
-      #   
-      #   req(input$plot_height)
-      #   req(!is.na(input$plot_height))
-      # 
-      #   plot(heatmap_obj()$gtable)
-      # }, 
-      # height = function(x) input$plot_height
-      # )
-      #   
-      # output$download_png <- downloadHandler(
-      #   filename = function() {
-      #     paste0("heatmap.png")
-      #   },
-      #   content = function(file) {
-      #     ggplot2::ggsave(
-      #       file, 
-      #       heatmap_obj()$gtable, 
-      #       device = "png",
-      #       width = input$shiny_width/4, ## 1pixel ~ 0.26mm at 96 dpi. it's ~0.35 at 72dpi
-      #       #height = input$shiny_height/4,
-      #       #width = input$plot_width*0.35,
-      #       height = input$plot_height*0.35,  
-      #       units = "mm"
-      #     )
-      #   }
-      # )
-      #  
-      # output$download_pdf <- downloadHandler(
-      #   filename = function() {
-      #     paste0("heatmap.pdf")
-      #   },
-      #   content = function(file) {
-      #     ggplot2::ggsave(
-      #       file, 
-      #       heatmap_obj(), 
-      #       device = "pdf",
-      #       width = input$shiny_width/4,
-      #       #width = input$plot_width*0.35,
-      #       height = input$plot_height*0.35,  
-      #       units = "mm"
-      #     )
-      #   }
-      # )    
+      
+      
+      ## data reactive expressions ----
+      # we could have tickboxes to select which samples are shown
+      selected_data <- reactive({
+        
+        validate(need(dataset(), "Please load a dataset"))
+        
+        if(input$selected_set == "all") {
+          return (dataset())
+        } else {
+          req(of_interest(), dataset())
+          genes_of_interest <- of_interest()[[input$selected_set]][[1]]
+          dataset()[rownames(dataset()) %in% genes_of_interest, ]
+        }
+      })
+      
+      heatmap_obj <- reactive({
+        validate(
+          need(
+            !any(is.na(selected_data())),
+            "Dataset contains NA or missing values so heatmap cannot be plotted."
+          )
+        )
+        pheatmap::pheatmap(
+          selected_data(),
+          scale = "row",
+          silent = TRUE
+        )
+      })
+
+      ## renderPlot ----
+      output$plot <- renderPlot({
+
+        req(input$plot_height)
+        req(!is.na(input$plot_height))
+
+        plot(heatmap_obj()$gtable)
+      }, height = function(x) input$plot_height)
+
+      ## download functions ----
+      output$download_png <- downloadHandler(
+        filename = function() {
+          paste0("heatmap.png")
+        },
+        content = function(file) {
+          ggplot2::ggsave(
+            file,
+            heatmap_obj()$gtable,
+            device = "png",
+            width = input$shiny_width/4, ## 1pixel ~ 0.26mm at 96 dpi. it's ~0.35 at 72dpi
+            #height = input$shiny_height/4,
+            #width = input$plot_width*0.35,
+            height = input$plot_height*0.35,
+            units = "mm"
+          )
+        }
+      )
+
+      output$download_pdf <- downloadHandler(
+        filename = function() {
+          paste0("heatmap.pdf")
+        },
+        content = function(file) {
+          ggplot2::ggsave(
+            file,
+            heatmap_obj(),
+            device = "pdf",
+            width = input$shiny_width/4,
+            #width = input$plot_width*0.35,
+            height = input$plot_height*0.35,
+            units = "mm"
+          )
+        }
+      )
       observeEvent(input$browser, browser())
     }
   )
